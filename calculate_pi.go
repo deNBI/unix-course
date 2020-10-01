a6891fbd4548db6f957790d6a32592fefb280ab0
Christian Henke <christian@cebitec.uni-bielefeld.de>
2020-10-01 17:35:10 +0200

    format go code

--- calculate_pi.go
+++ calculate_pi.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"time"
 )
 
 func main() {
-  var pi float64 = 0.0
-  for i := 1.0; i <= 1e12; i+=4.0 {
-    pi += 4.0/i;
-    pi -= 4.0/(i+2.0);
-    if (int(i) % 1e9 == 1 || i < 30) {
-      fmt.Printf("Approximation after %12d steps: %.26f\n", int(i), pi);
-    }
-    if (i < 30) {
-      time.Sleep(1*time.Second)
-    }
-  }
+	var pi float64 = 0.0
+	for i := 1.0; i <= 1e12; i += 4.0 {
+		pi += 4.0 / i
+		pi -= 4.0 / (i + 2.0)
+		if int(i)%1e9 == 1 || i < 30 {
+			fmt.Printf("Approximation after %12d steps: %.26f\n", int(i), pi)
+		}
+		if i < 30 {
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
